a8e6dc876c6c568362779f4442a51c1b5ccbcf86
Soumya Deb <debloper@gmail.com>
2012-06-29 18:30:51 +0530

    Cleaning up multi-client handling

--- config.php
+++ config.php
@@ -1,5 +1,33 @@
 <?php
+
+/**
+ * Conditionally put more configs, to handle multiple different clients
+ * The $_REQUEST['<submit_button_value>'] will be single & unique
+ * Each client has to use an unique key for the submit request,
+ * based on which, the correct configuration will be chosen
+ * (i.e. that particular IF block executes loading values)
+**/
+
 if (isset($_REQUEST['test'])) {
-	$data = array('name','sender','subject','message','deb.webdev@gmail.com','test@mailer.d','http://google.com');
+	$data = array(
+		'name',
+		'sender',
+		'subject',
+		'message',
+		'id@mail.com',
+		'test@mailer.d',
+		'http://localhost/'
+	);
+}
+if (isset($_REQUEST['real'])) {
+	$data = array(
+		'name',
+		'sender',
+		'subject',
+		'message',
+		'deb.webdev@gmail.com',
+		'test@mailer.d',
+		'http://google.com'
+	);
 }
 ?>
--- producer.php
+++ producer.php
@@ -1,7 +1,11 @@
 <?php
 $mailto = $got_to;
 $subject = "Testing mailer.d : ". $got_subject;
-$content = "Test Phase 10.abstraction :\r\n"."\r\nName :". $got_name ."\r\nEmail :". $got_email ."\r\nMessage Body :\r\n". $got_message;
-$headers = "From: ". $got_from ."\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=utf-8\r\n";
+$content = "Test Phase 10.abstraction :\r\n"
+			."\r\nName :". $got_name
+			."\r\nEmail :". $got_email
+			."\r\n\r\n\r\nMessage Body :\r\n\r\n". $got_message;
+$headers = "From: ". $got_from ."\r\nMIME-Version: 1.0\r\n"
+			."Content-Type: text/plain; charset=utf-8\r\n";
 $relocate = "Location: ". $got_redirect;
 ?>
